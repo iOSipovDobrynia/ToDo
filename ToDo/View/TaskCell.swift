4f93e4bfba6da08efb137c79e911b8cbcd344b85
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-10-22 19:40:41 +0300

    complete lessons 21, 22

--- ToDo/View/TaskCell.swift
+++ ToDo/View/TaskCell.swift
@@ -8,7 +8,22 @@
 import UIKit
 
 class TaskCell: UITableViewCell {
+    
+    // MARK: - IBOutlets
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+    // MARK: - Public methods
     func configure(with task: Task) {
+        titleLabel.text = task.title
         
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE, MMM d, yyyy"
+        dateLabel.text = dateFormatter.string(from: task.date)
+        
+        if let locationName = task.location?.name {
+            locationLabel.text = locationName
+        }
     }
 }
---
+++ ToDoTests/View/TaskCellTests.swift
@@ -0,0 +1,96 @@
+//
+//  TaskCellTests.swift
+//  ToDoTests
+//
+//  Created by Goodwasp on 22.10.2023.
+//
+
+import XCTest
+@testable import ToDo
+
+final class TaskCellTests: XCTestCase {
+    
+    var cell: TaskCell!
+
+    override func setUpWithError() throws {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: String(describing: TaskListViewController.self)) as! TaskListViewController
+        
+        vc.loadViewIfNeeded()
+        
+        let tableView = vc.tableView
+        let dataSource = FakeDataSource()
+        tableView?.dataSource = dataSource
+        
+        cell = tableView?.dequeueReusableCell(withIdentifier: String(describing: TaskCell.self), for: IndexPath(row: 0, section: 0)) as? TaskCell
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+
+    func testHasTitleLabel() {
+        XCTAssertNotNil(cell.titleLabel)
+    }
+    
+    func testCellHasTitleLabelInContentView() {
+        XCTAssertTrue(cell.titleLabel.isDescendant(of: cell.contentView ))
+    }
+    
+    func testHasLocationLabel() {
+        XCTAssertNotNil(cell.locationLabel)
+    }
+    
+    func testCellHasLocationLabelInContentView() {
+        XCTAssertTrue(cell.locationLabel.isDescendant(of: cell.contentView ))
+    }
+    
+    func testHasDateLabel() {
+        XCTAssertNotNil(cell.dateLabel)
+    }
+    
+    func testCellHasDateLabelInContentView() {
+        XCTAssertTrue(cell.dateLabel.isDescendant(of: cell.contentView ))
+    }
+    
+    func testConfigureSetsTitle() {
+        let task = Task(title: "Foo")
+        cell.configure(with: task)
+        
+        XCTAssertEqual(cell.titleLabel.text, task.title)
+    }
+    
+    func testConfigureSetsDate() {
+        let task = Task(title: "Foo")
+        cell.configure(with: task)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE, MMM d, yyyy"
+        
+        let date = task.date
+        let dateString = dateFormatter.string(from: date)
+        
+        XCTAssertEqual(cell.dateLabel.text, dateString)
+    }
+    
+    func testConfigureSetsLocation() {
+        let task = Task(title: "Foo", location: Location(name: "Bar"))
+        cell.configure(with: task)
+        
+        XCTAssertEqual(cell.locationLabel.text, task.location?.name)
+    }
+}
+
+extension TaskCellTests {
+    class FakeDataSource: NSObject, UITableViewDataSource {
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            1
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            UITableViewCell()
+        }
+        
+        
+    }
+}
